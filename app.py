1b2384a473037b6860e6f62867636cfebdb42563
Joseph Key <keymonkey@pm.me>
2020-03-30 18:06:17 +0700

    added

--- app.py
+++ app.py
@@ -1,6 +1,84 @@
-# add change
-
 import math
+
+
+# Day 1
+
+# x = input("x: ")
+y = int(x) + 3
+print(y)
+
+temp = 25
+if temp < 20:
+    print("it's cold!")
+elif temp > 30:
+    print("it's crazy hot!!")
+else:
+    print("it's fine")
+print("ok")
+
+age = 24
+if age < 18:
+    print("too young!")
+else:
+    print("ok!")
+
+# OR
+age = 12
+
+message = ("ok") if age > 18 else "too young"
+
+print(message)
+
+# and  not  or
+
+stack_sats = True
+strong_hands = True
+big_ego = False
+
+if (stack_sats or strong_hands) and not big_ego:
+    print("to the moon!")
+else:
+    print("sad face...")
+
+
+# coding bitcoin
+
+shit_coin = False
+strong_hands = False
+big_heart = False
+
+if (strong_hands or big_heart) and not shit_coin:
+    print("to the moon!")
+else:
+    print("game over")
+
+    # chaining comparison operators
+
+temp = 76
+
+if 50 < temp <= 75:
+    print("it's a nice day")
+else:
+    print("it's too hot or too cold")
+
+# for  in a range
+
+for number in range(1, 100, 2):
+    print("Try", number, number * "*")
+
+# maybe trying to get user info OR send something
+
+successful_sent = True
+
+for number in range(1, 4):
+    print("Try", number)
+    if successful_sent:
+        print("thanks")
+        break
+else:
+    print("sorry, no go")
+
+# Day 0
 
 print("Hello World !")
 print("*" * 10)
@@ -42,8 +120,3 @@ print("Jay" in full)            # True
 print("Joe" not in full)        # True
 
 print(math.ceil(2))
-
-
-x = input("x: ")
-y = int(x) + 3
-print(y)
